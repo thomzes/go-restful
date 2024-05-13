7c9a17a78e477132d448457265596a646cf01492
thomzes <thomz161@gmail.com>
2024-05-14 00:27:05 +0700

    feat: create unit test for all functions

--- main.go
+++ main.go
@@ -6,10 +6,8 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/julienschmidt/httprouter"
 	"github.com/thomzes/go-restful/app"
 	"github.com/thomzes/go-restful/controller"
-	"github.com/thomzes/go-restful/exception"
 	"github.com/thomzes/go-restful/helper"
 	"github.com/thomzes/go-restful/middleware"
 	"github.com/thomzes/go-restful/repository"
@@ -23,16 +21,7 @@ func main() {
 	categoryRepository := repository.NewCategoryRepository()
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
-
-	router := httprouter.New()
-
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
